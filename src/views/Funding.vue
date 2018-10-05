a4ac8b1ab83e433466486dbb1d01823c7b7f5aa3
Shirshak <shirshak55@gmail.com>
2018-10-05 15:13:31 +0545

    Add Carousel

--- src/views/Funding.vue
+++ src/views/Funding.vue
@@ -1,14 +1,29 @@
 <template>
+    <section class="funding">
+        <div class="container mx-auto px-4 pt-16 pb-12 text-center ">
+            <h1 class="mb-10">Funding</h1>
 
-    <div class="container mx-auto px-4 pt-16 pb-12 text-center ">
-        <h1 class="mb-10">Funding</h1>
+            <p>We would like to thank Cern For funding super computer project</p>
+        </div>
+        <carousel :autoplay="true" :loop="true">
+            <slide>
+                Slide 1 Content
+            </slide>
+            <slide>
+                Slide 2 Content
+            </slide>
+        </carousel>
 
-        <p>We would like to thank Cern For funding super computer project</p>
-    </div>
+    </section>
 </template>
 
 <script>
+import { Carousel, Slide } from "vue-carousel";
 export default {
-  name: "Funding"
+  name: "Funding",
+  components: {
+    Carousel,
+    Slide
+  }
 };
 </script>
