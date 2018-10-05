cd93584d5a190289d40f642eee1b62225426cc04
Shirshak <shirshak55@gmail.com>
2018-10-05 12:25:30 +0545

    Blue is our color

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,13 +3,13 @@
         <div class="container px-4 text-center max-w-lg mx-auto">
             <ul class="list-reset flex justify-center flex-wrap">
                 <li class="m-3">
-                    <a class="text-red-lighter hover:text-red-lightest font-semibold no-underline" href="https://ku.edu.np">KU</a>
+                    <a class="text-red-lighter hover:text-blue-lightest font-semibold no-underline" href="https://ku.edu.np">KU</a>
                 </li>
                 <li class="m-3">
-                    <a class="text-red-lighter hover:text-red-lightest font-semibold no-underline" href="https://github.com/kuhpc">Github</a>
+                    <a class="text-red-lighter hover:text-blue-lightest font-semibold no-underline" href="https://github.com/kuhpc">Github</a>
                 </li>
                 <li class="m-3">
-                    <a class="text-red-lighter hover:text-red-lightest font-semibold no-underline" href="https://facebook.com/kuhpc">Facebook</a>
+                    <a class="text-red-lighter hover:text-blue-lightest font-semibold no-underline" href="https://facebook.com/kuhpc">Facebook</a>
                 </li>
             </ul>
             <div class="text-white leading-normal no-underline">Copyright © KUHPC.</div>
