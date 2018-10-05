39c2f2a348f4890a980bd2dd9d9888ecd36165c9
Shirshak <shirshak55@gmail.com>
2018-10-05 17:09:11 +0545

    minor fixes

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -6,16 +6,16 @@
                 <p> You can contact us directly using following method:</p>
                 <div class="flex flex-wrap-reverse lg:flex-row justify-center lg:my-4 py-6 pb-16">
                     <div class="w-full lg:w-1/4 text-left mx-8 pt-10">
-                        <h3 class="font-light text-indigo-darker font-bold text-3xl tracking-tight">1. Sign up online</h3> <p class="leading-tight py-10 tracking-wide">Simply Email Us at <code>kuhpc@ku.edu.np</code></p>
+                        <h3 class="font-light text-indigo-darker  text-3xl tracking-tight">1. Email Us</h3> <p class="leading-tight py-10 tracking-wide">Simply Email Us at <code>kuhpc@ku.edu.np</code></p>
                     </div>
 
-                    <div class="font-light w-full lg:w-1/4 text-left mx-8 pt-10"><h3 class="text-indigo-darker font-bold text-3xl tracking-tight">2. Visit US</h3> <p class="leading-tight py-10 tracking-wide">Visit Us at Kathmandu University Block 10</p></div>
+                    <div class="font-light w-full lg:w-1/4 text-left mx-8 pt-10"><h3 class="font-light text-3xl tracking-tight">2. Visit US</h3> <p class="leading-tight py-10 tracking-wide">Visit Us at Kathmandu University Block 10</p></div>
 
-                    <div class="font-light w-full lg:w-1/4 text-left mx-8 pt-10"><h3 class="text-indigo-darker font-bold text-3xl tracking-tight">3. Mail Us</h3> <p class="leading-tight py-10 tracking-wide">You can send post mail at Kathmandu Univeristy </p></div>
+                    <div class="font-light w-full lg:w-1/4 text-left mx-8 pt-10"><h3 class="font-light text-3xl tracking-tight">3. Mail Us</h3> <p class="leading-tight py-10 tracking-wide">You can send post mail at Kathmandu Univeristy </p></div>
 
                 </div>
 
-                <p class="mb-4">You can find our location via following map:</p>
+                <p class="mb-8">You can find our location via following map:</p>
 
                 <div style="width: 100%"><iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=27.61866315,85.53822627738802&amp;q=Kathmandu%20Universtiy+(Kathmandu%20University)&amp;ie=UTF8&amp;t=&amp;z=17&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/create-google-map/">Embed Google Map</a></iframe></div><br />
 
